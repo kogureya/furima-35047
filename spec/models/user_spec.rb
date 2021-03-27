fd5c2b6a4198503047bf2807aa7417a54256ffff
kogureya <gurelio5908@gmail.com>
2021-03-27 10:01:26 +0900

    Gem導入箇所修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
+  describe '新規登録' do
+    context '新規登録できる時' do
+      it '必要なデータが全て存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordが６文字以上で英数字混在であれば登録できる' do
+        @user.password = 'aaa111'
+        @user.password_confirmation = 'aaa111'
+      end
+    end
+  end
 end
