9c67c3d77c35a78b94dcd14fa6ef23a216484d66
kogureya <gurelio5908@gmail.com>
2021-04-01 11:20:56 +0900

    制限をupdateアクションにも適用、before_actionに統合

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :not_signed_in, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
@@ -52,5 +52,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def not_signed_in
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 end
