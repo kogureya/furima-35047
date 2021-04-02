65ad2ba5711346413defb9e8d46f4e98b3293cb3
kogureya <gurelio5908@gmail.com>
2021-04-02 18:18:49 +0900

    商品購入機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :block_number, :building, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture, :city, :block_number, :building, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :prefecture, :city, :block_number, :order_id, :user_id, :item_id
+    validates :city, :block_number, :user_id, :item_id, :token
 
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, length: { maximum: 11 }
-
+    validates :prefecture, numericality: { other_than: 1, message: 'select' }
 
   end
 
@@ -15,7 +15,6 @@ class OrderAddress
     order = Order.create(item_id: item_id, user_id: user_id)
     address = Address.create(postal_code: postal_code, prefecture_id: prefecture, city: city, block_number: 
       block_number, building: building, phone_number: phone_number, order_id: order.id)
-
   end
   
 
